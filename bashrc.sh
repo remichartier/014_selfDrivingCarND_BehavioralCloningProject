e33c5c982a7df4e61a2d4175d1c777b5dd0b0795
remichartier <remipr.chartier@gmail.com>
2020-12-05 10:03:08 +0000

    update

---
+++ bashrc.sh
@@ -0,0 +1,4 @@
+git config --global user.email "remipr.chartier@gmail.com"
+git config --global user.name "remichartier"
+git remote set-url origin "https://github.com/remichartier/014_selfDrivingCarND_BehavioralCloningProject"
+
