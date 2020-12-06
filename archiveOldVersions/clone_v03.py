7759d31e2ecba4fd4787716114164aa806e86e87
remichartier <remipr.chartier@gmail.com>
2020-12-06 12:18:27 +0000

    update

---
+++ archiveOldVersions/clone_v03.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import numpy as np
+import cv2
+
+from commonFunctions_v03 import get_info_from_logfile
+
+# History
+# v01 : Start
+# v02 : add nb_images to read parameter
+# v03 : add normalization + mean centering data to 0
+
+images, measurements = get_info_from_logfile(nb_images=100)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+#print(f'X_train shape : {X_train.shape}')
+#print(f'images shape : {im.shape}')
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.callbacks import ModelCheckpoint,EarlyStopping
+
+model = Sequential()
+model.add(Lambda(lambda x: ((x/255) - 0.5),input_shape=(160,320,3)))
+model.add(Flatten())
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+# Callbacks to save best model and prevent overfit by early stopping 
+checkpoint = ModelCheckpoint(filepath='bestModelFolder/model.{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss', save_best_only=True)
+stopper = EarlyStopping(monitor='val_loss', min_delta=0.0003, patience=5)
+# model.fit(callbacks=[checkpoint, stopper])
+model.fit(X_train,y_train, validation_split=0.2, shuffle = True, epochs=10, callbacks=[checkpoint, stopper])
+
+model.save('model.h5')
---
+++ commonFunctions_v03.py
@@ -0,0 +1,56 @@
+import os
+import csv
+import cv2
+
+from scipy import ndimage
+
+# list of common functions
+# from commonFunctions_vxx import get_log_path
+# from commonFunctions_vxx import get_lines_logfile 
+# from commonFunctions_vxx import get_info_from_lines
+# from commonFunctions_vxx import get_info_from_logfile
+# from commonFunctions_vxx import
+
+# History
+# v01 : Start
+# v02 : add nb_images to read parameter
+# v03 : from scipy import ndimage, due to cv2.imread will get images in BGR format, while drive.py uses RGB. In the video above one way you could keep the same image formatting is to do "image = ndimage.imread(current_path)"
+
+driving_log_file = 'driving_log.csv'
+
+# Select right sample data folder whether in GPU mode or not
+# check if ./data/driving_log.csv exists otherwise select 
+# simulationData/001_1stTrackSampleDrivingData/
+def get_log_path() :
+    if os.path.exists("./data/" + driving_log_file) :
+        return("./data/")
+    else : 
+        return("./simulationData/001_1stTrackSampleDrivingData/")
+
+
+def get_lines_logfile() :
+    l = []
+    with open (get_log_path() + driving_log_file ) as csv_file :
+        reader = csv.reader(csv_file)
+        for line in reader :
+            l.append(line)
+    return l
+
+
+def get_info_from_lines(l,nb_images=None) :
+    imgs = []
+    meas = []
+    log_path = get_log_path()
+    print('Function get_info_from_lines() : Load images ... Please wait ....')
+    for line in l[1:nb_images] :
+        #image = cv2.imread(log_path + line[0].strip())
+        image = ndimage.imread(log_path + line[0].strip())
+        imgs.append(image)
+        measurement = float(line[3])
+        meas.append(measurement)
+    print('Function get_info_from_lines() : Images loaded')
+    return imgs,meas
+
+def get_info_from_logfile(nb_images=None) :
+    lines = get_lines_logfile()
+    return get_info_from_lines(lines,nb_images)
