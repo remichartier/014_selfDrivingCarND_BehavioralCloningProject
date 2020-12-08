47a0122794acd234a74bca54a9f104ce1f560961
remichartier <remipr.chartier@gmail.com>
2020-12-08 09:52:41 +0000

    update

--- model_v10.py
+++ model_v10.py
@@ -83,7 +83,7 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 # Callbacks to save best model and prevent overfit by early stopping 
 checkpoint = ModelCheckpoint(filepath='bestModelFolder/model.{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss', save_best_only=True)
-stopper = EarlyStopping(monitor='val_loss', min_delta=0.0003, patience=10)
+stopper = EarlyStopping(monitor='val_loss', min_delta=0.0003, patience=3)
 # model.fit(callbacks=[checkpoint, stopper])
 history_object = model.fit(X_train,y_train, batch_size, validation_split=0.2, shuffle = True, epochs=10, callbacks=[checkpoint, stopper])
 
