c9f2b1cb739132135b5d2cb69249a25a78f90493
remichartier <remipr.chartier@gmail.com>
2020-12-31 08:23:09 +0000

    add fix steering correction

--- bashrc.sh
+++ bashrc.sh
@@ -1,4 +1,6 @@
 git config --global user.email "remipr.chartier@gmail.com"
 git config --global user.name "remichartier"
 git remote set-url origin "https://github.com/remichartier/014_selfDrivingCarND_BehavioralCloningProject"
-
+#git remote set-url origin "git@github.com:remichartier/014_selfDrivingCarND_BehavioralCloningProject"
+#eval "$(ssh-agent -s)"
+#ssh-add .ssh/20201224_1219_github_ed25519
